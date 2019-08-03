84b10817b13d766a763a8b0f11b5a20618a1211c
Fladoodlebob <marrj@sheridancollege.ca>
2019-08-02 20:11:44 -0400

    Add files via upload

---
+++ github/assets/data/myBooks.ts
@@ -0,0 +1,40 @@
+import {Books} from 'src/app/books';
+
+
+export const MYBOOKS: Books[] =
+[
+  
+                  {      
+                    bookName:"The House of Hades",
+                    authorName:"Rick Riordan",
+                    genre: "Fantasy",
+                    published:2013,
+                    picture:"../images/houseOfHades.jpg"},
+
+                  {    
+                    bookName:"Scorpia Rising",
+                    authorName:"Anthony Horowitz",
+                    genre:"Spy Thriller",
+                    published:2011,
+                    picture:"../images/scorpiaRising.jpg"
+                    },
+                    {
+                    bookName:"Fate/Zero",
+                    authorName:"Gen Urobuchi",
+                    genre:"Dark Fantasy",
+                    published:2005,
+                    picture:"../images/fateZero.jpg"
+                     },
+                  {
+                    bookName:"Beyond the Shadows",
+                    authorName:"Brent Weeks",
+                    genre:"Fantasy",
+                    published:2008,
+                    picture:"../images/beyondTheShadows.jpg"
+                      }
+     
+
+
+
+]
+
